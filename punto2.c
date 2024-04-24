3e69857d303c064250ee6c2933939ce5f9ea7bc7
Guillermo Simon <guillermosimon.2018@gmail.com>
2024-04-24 19:12:21 -0300

    se agrega el punto2.c funcional-incompleto

--- punto2.c
+++ punto2.c
@@ -81,6 +81,108 @@ void mostrarLista(Nodo **start){
     
 }
 
+Nodo buscarTarea(Nodo** start, int ID){
+    Nodo* buscada =malloc(sizeof(Nodo));
+    buscada = *start;
+    while (buscada->T.tareaID != ID)
+    {
+        buscada = buscada->siguiente;
+    }
+
+    return *buscada;
+}
+
+void quitarTarea(Nodo**start, int ID){
+    Nodo** aux = &(*start);
+    while (*aux!=NULL && ((*aux)->T.tareaID != ID))
+    {
+        aux = &(*aux)->siguiente;
+    }
+    
+    if (aux)
+    {   
+        Nodo* temp= *aux;
+        *aux = (*aux)->siguiente;
+        free(temp);   
+    }
+    
+}
+
+void buscarPorId(Nodo** startPend, Nodo** startReal, int ID){
+    //NO ENTIENDO POR QUE REPINGO NO FUNCIONA
+    Nodo* buscarPend = *startPend;
+    Nodo* buscarReal = *startReal;
+    int bandera = 0;
+    while (buscarPend != NULL && bandera == 0)
+    {
+        if (buscarPend->T.tareaID == ID)
+        {
+            printf("\nID: %d\n", buscarPend->T.tareaID);
+            printf("Descripcion: ");
+            puts(buscarPend->T.Descripcion);
+            printf("Duracion: %d\n", buscarPend->T.Duracion);
+            bandera = 1;
+        }
+        buscarPend = buscarPend->siguiente;
+    }
+    
+    if (bandera==0)
+    {
+        while (buscarReal != NULL && bandera ==0)
+        {
+            if (buscarReal->T.tareaID == ID)
+            {
+                printf("\nID: %d\n", buscarReal->T.tareaID);
+                printf("Descripcion: ");
+                puts(buscarReal->T.Descripcion);
+                printf("Duracion: %d\n", buscarReal->T.Duracion);
+                bandera = 1;
+            }
+
+            buscarReal = buscarReal->siguiente;
+        }
+    }else
+    {
+        puts("No se encontro");
+    }
+    
+}
+
+void buscarPorPalabra(Nodo** startPend, Nodo** startReal, char frase[]){
+    Nodo* buscarPend = *startPend;
+    Nodo* buscarReal = *startReal;
+    int bandera = 0;
+    while (buscarPend != NULL && bandera == 0)
+    {
+        if (strstr(buscarPend->T.Descripcion, frase))
+        {
+            printf("\nID: %d\n", buscarPend->T.tareaID);
+            printf("Descripcion: ");
+            puts(buscarPend->T.Descripcion);
+            printf("Duracion: %d\n", buscarPend->T.Duracion);
+            bandera = 1;
+        }
+        buscarPend = buscarPend->siguiente;
+    }
+    if (bandera == 0)
+    {
+        while (buscarReal != NULL && bandera == 0)
+        {
+            if (strstr(buscarReal->T.Descripcion, frase))
+            {
+                printf("\nID: %d\n", buscarReal->T.tareaID);
+                printf("Descripcion: ");
+                puts(buscarReal->T.Descripcion);
+                printf("Duracion: %d\n", buscarReal->T.Duracion);
+                bandera = 1;    
+            }          
+        }
+        buscarReal = buscarReal->siguiente;
+    }
+    
+    
+}
+
 int main(){
 
     srand(time(NULL));
@@ -102,8 +204,30 @@ int main(){
         scanf("%d", &seguir);
 
     }
-    mostrarLista(&startPendientes); 
 
+    mostrarLista(&startPendientes);
+    int IDBuscado;
+    printf("\nSeleccionar el ID a mover: ");
+    fflush(stdin);
+    scanf("%d", &IDBuscado);
+    Nodo auxiNodo =buscarTarea(&startPendientes, IDBuscado);
+    quitarTarea(&startPendientes, IDBuscado);
+    insertarNodo(&startRealizadas, &auxiNodo);
+
+    puts("Lista de pendientes:");
+    mostrarLista(&startPendientes);
+    puts("--------------");
+    puts("Lista de realizadas"); 
+    mostrarLista(&startRealizadas);
+
+
+    printf("\n");
+    puts("Ingrese el ID a buscar: ");
+    fflush(stdin);
+    scanf("%d", &IDBuscado);
+    buscarPorId(&startPendientes, &startRealizadas, IDBuscado);
+    
+    buscarPorPalabra(&startPendientes, &startRealizadas, "simoca");
     
     
   
